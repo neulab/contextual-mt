3e1e355c3aa0d58df277f71bf04e2356f86b616c
Patrick Fernandes <pattuga@gmail.com>
2020-09-18 12:17:05 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="dialogue-mt",  # Replace with your own username
+    name="dialogue-mt", 
     version="0.0.1",
     author="Patrick Fernandes, Kayo Yin, Kervy Dante",
     author_email="pfernand@cs.cmu.edu",
     description="Package containing experiments related to Dialogue Translation",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/neulab/dialogue-mt",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
