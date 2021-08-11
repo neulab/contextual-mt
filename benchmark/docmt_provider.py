f5f97225c188dec637ecfa9a3f5deca24a4ddd91
Patrick Fernandes <pattuga@gmail.com>
2021-08-11 09:58:35 -0400

    Update benchmarker

--- benchmark/docmt_provider.py
+++ benchmark/docmt_provider.py
@@ -1,48 +1,16 @@
 from typing import List
 from abc import ABC, abstractmethod
 import argparse
+import requests
 
 from contextual_mt.utils import parse_documents
 
 import tqdm
- 
-from nltk.tokenize.util import xml_unescape, xml_escape
-
-STOP_TOKEN = " 🛑 "
 
 class DocumentMTProvider(ABC):
     def __init__(self, source_lang, target_lang):
         self.source_lang = source_lang
         self.target_lang = target_lang
-
-    def unscape_and_split_source(self, source):
-        """ """
-        sentences = [xml_unescape(s) for s in source]
-        splited_sentences =  [s.text for s in self.source_nlp(" ".join(sentences)).sents]
-        return splitted_sentences, None
-
-        mapping = []
-        splitted_sentences = []
-        for i, s in enumerate(sentences):
-            splitted_s = [s.text for s in self.source_nlp(s).sents]
-            splitted_sentences.extend(splitted_s)
-            mapping.extend([i for i in splitted_s])
-        return splitted_sentences, mapping
-
-
-    def escape_and_desplit(self, splitted_target, mapping):
-        sentences = [xml_escape(s) for s in target]
-        prev = None
-        target = []
-        for i, s in zip(mapping, sentences):
-            if i != prev:
-                if prev is not None:
-                    target.append(" ".join(curr_t))
-                curr_t = []
-            curr_t.append(s)
-            prev = i
-        return target
-
 
     def translate_document(self, source: List[str], context_size: int=None):
         if context_size is not None:
@@ -51,18 +19,22 @@ class DocumentMTProvider(ABC):
                 start_idx = max(i-context_size, 0)
                 end_idx = i+1
                 partial_source = source[start_idx:end_idx]
-                output = self._translate(STOP_TOKEN.join([xml_unescape(s) for s in partial_source]))
-                partial_target = [xml_escape(s) for s in output.split(STOP_TOKEN)]
+                output = self._translate(self.STOP_TOKEN.join(partial_source))
+                partial_target = output.split(self.STOP_TOKEN)
                 if len(partial_source) != len(partial_target):
                     import ipdb; ipdb.set_trace()
                 target.append(partial_target[-1])
         else:
-            source_str = STOP_TOKEN.join([xml_unescape(s) for s in source])
+            source_str = self.STOP_TOKEN.join(source)
             target_str = self._translate(source_str)
-            target = [xml_escape(s) for s in target_str.split(STOP_TOKEN)]
+            target = target_str.split(self.STOP_TOKEN)
         
         if len(source) != len(target):
-            import ipdb; ipdb.set_trace()
+            if context_size == 0:
+                raise ValueError("something is wrong")
+
+            print("document couldn't be translated with context-aware; trying sentence-level")
+            return self.translate_document(source, context_size=0)
 
         return target
 
@@ -75,17 +47,41 @@ class DocumentMTProvider(ABC):
 class FreeGoogleProvider(DocumentMTProvider):
     def __init__(self, source_lang, target_lang):
         super().__init__(source_lang, target_lang)
-
+        self.STOP_TOKEN = " 🛑."
         from googletrans import Translator
         self.translator = Translator()
 
-    def _translate(self, source_str:str):
+    def _translate(self, source_str: str):
         return self.translator.translate(source_str, src=self.source_lang, dest=self.target_lang).text
+
+
+class DeepLProvider(DocumentMTProvider):
+    def __init__(self, source_lang, target_lang, api_key):
+        super().__init__(source_lang, target_lang)
+        self.STOP_TOKEN = "<x>STOP</x>"
+        self.api_key = api_key
+
+    def _translate(self, source_str: str):
+        result = requests.post("https://api.deepl.com/v2/translate", 
+            data={
+                "source_lang": self.source_lang.upper(),
+                "target_lang": self.target_lang.upper(),
+                "text": source_str,
+                "auth_key": self.api_key,
+                "tag_handling": "xml",
+                "ignore_tags": "x"
+        })
+        try:
+            return result.json()['translations'][0]['text']
+        except KeyError:
+            print(result)
+            raise Exception()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--provider", default="free_google")
+    parser.add_argument("--api-key", default=None)
     parser.add_argument("--source-file")
     parser.add_argument("--docids-file")
     parser.add_argument("--translations-file")
@@ -94,16 +90,26 @@ def main():
     args = parser.parse_args()
 
     # load files needed
-    with open(args.source_file, "r") as src_f:
+    with open(args.source_file, "r", encoding='utf-8') as src_f:
         srcs = [line.strip() for line in src_f]
-    with open(args.docids_file, "r") as docids_f:
+    with open(args.docids_file, "r", encoding='utf-8') as docids_f:
         docids = [int(idx) for idx in docids_f]
 
     refs = [None for _ in srcs]
     documents = parse_documents(srcs, refs, docids)
 
     if args.provider == "free_google":
+        if args.target_lang == "zh_cn":
+            args.target_lang = "zh-CN"
+        if args.target_lang == "pt_br":
+            args.target_lang = "pt"
         provider = FreeGoogleProvider(args.source_lang, args.target_lang)
+    elif args.provider == "deepl":
+        if args.target_lang == "zh_cn":
+            args.target_lang = "zh"
+        if args.target_lang == "pt_br":
+            args.target_lang = "pt-br"
+        provider = DeepLProvider(args.source_lang, args.target_lang, args.api_key)
     else:
         raise ValueError("unknown provider")
 
@@ -114,7 +120,7 @@ def main():
 
     assert len(translations) == len(srcs)
 
-    with open(args.translations_file, "w") as f:
+    with open(args.translations_file, "w", encoding='utf-8') as f:
         for translation in translations:
             print(translation, file=f)
 
